2dc9376869676197e8c5be5cce60ff98e6494394
Bhuvi-523 <bhuvaneswari.g523@gmail.com>
2024-08-10 15:02:52 +1000

    Update ResponseHeaderInjector.java

--- src/main/java/com/audition/configuration/ResponseHeaderInjector.java
+++ src/main/java/com/audition/configuration/ResponseHeaderInjector.java
@@ -1,10 +1,23 @@
 package com.audition.configuration;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 @Component
 public class ResponseHeaderInjector {
 
-    // TODO Inject openTelemetry trace and span Ids in the response headers.
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        // Get the current active span
+        Span currentSpan = Span.current();
+        SpanContext spanContext = currentSpan.getSpanContext();
 
+        // Inject the trace ID and span ID into the response headers
+        response.addHeader("trace-id", spanContext.getTraceId());
+        response.addHeader("span-id", spanContext.getSpanId());
+    }
 }
